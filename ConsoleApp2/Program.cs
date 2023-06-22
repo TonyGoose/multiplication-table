778233ce0cecc2757cc65ea25340c7b8bc4df62e
Tony <tony.goose@yandex.ru>
2023-06-22 17:54:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N times N multiplication table with array-nested loops - !press enter a number for the table!");
+
+            int n = Int32.Parse(Console.ReadLine()); ; //multiplication table dimension
+
+            int[,] q = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    q[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"\t{i + 1}");
+            }
+
+            Console.WriteLine();//insert a newline
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i + 1}");
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"\t{q[j, i]}");
+                }
+                Console.WriteLine();//insert a newline
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
